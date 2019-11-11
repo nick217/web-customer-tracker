2670e8feea30746cb8ebdfa9a56364b59d68ea7c
Nikhil Raheja <nick170294@gmail.com>
2019-11-12 01:57:43 +0530

    deleting data from db

--- src/com/practice/springdemo/dao/CustomerDAOImpl.java
+++ src/com/practice/springdemo/dao/CustomerDAOImpl.java
@@ -60,4 +60,18 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return theCustomer;
 	}
 
+	@Override
+	public void deleteCustomer(int theId) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// now delete the customer from database using primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+
+		theQuery.executeUpdate();
+
+	}
+
 }
